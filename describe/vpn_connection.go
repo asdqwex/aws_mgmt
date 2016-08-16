a3e9b60f5ed0843e835e7c0167a9b7ebc405800d
Matthew Ellsworth <matthew.ellsworth@clickbank.com>
2016-08-16 10:29:52 -0600

    adds describe for all services of interest

---
+++ describe/vpn_connection.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+"fmt"
+
+"github.com/aws/aws-sdk-go/aws"
+"github.com/aws/aws-sdk-go/service/ec2"
+"github.com/aws/aws-sdk-go/aws/session"
+)
+
+func main() {
+
+  sess, err := session.NewSession()
+  if err != nil {
+    fmt.Println("failed to create session,", err)
+    return
+  }
+
+  svc := ec2.New(sess)
+
+	params := &ec2.DescribeVpnConnectionsInput{
+	  DryRun: aws.Bool(false),
+	}
+	resp, err := svc.DescribeVpnConnections(params)
+
+	if err != nil {
+	  // Print the error, cast err to awserr.Error to get the Code and
+	  // Message from an error.
+	  fmt.Println(err.Error())
+	  return
+	}
+
+	// Pretty-print the response data.
+	fmt.Println(resp)
+}
